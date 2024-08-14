2b0f2a984377198dc17e89085fff93b2709a3025
Leonardo Capristo <leonardo.capristo@hotmail.com>
2024-08-13 23:52:36 -0300

    ex2

--- Exercicios/2.sql
+++ Exercicios/2.sql
@@ -18,3 +18,30 @@ SELECT
         ELSE 0
 	END as menopausa
 FROM tb_dados2;
+
+
+#2- [Desafio] Crie uma nova coluna chamada posicao_tumor concatenando as colunas inv_nodes e quadrante.
+
+ALTER TABLE tb_dados2
+ADD COLUMN posicao_tumor VARCHAR(255);
+
+UPDATE tb_dados2
+SET posicao_tumor = CONCAT(inv_nodes, '-', quadrante);
+
+select * from tb_dados2;
+
+# 3 Crie um novo dataset com todas as variáveis após as transformações anteriores.
+
+tb_dados3create table tb_dados3
+SELECT 
+	CASE 
+		WHEN menopausa = 'premeno' THEN 1 
+		WHEN menopausa = 'ge40' THEN 2
+        WHEN menopausa = 'lt40' THEN 3 
+        ELSE 0
+	END as menopausa
+FROM tb_dados2;
+
+
+
+
